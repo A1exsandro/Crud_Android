b2b3ac0313387246cc3835c748360690fd6b7575
A1exsandro <alexsandro.acp@gmail.com>
2022-12-04 14:28:54 -0400

    crud

--- app/src/main/java/com/example/crud_android/database/DatabaseHelper.kt
+++ app/src/main/java/com/example/crud_android/database/DatabaseHelper.kt
@@ -1,8 +1,11 @@
 package com.example.crud_android.database
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.crud_android.model.TaskListModel
 
 class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DB_NAME,null, DB_VERSION) {
 
@@ -17,12 +20,75 @@ class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DB_NAME,null, 
 
     override fun onCreate(p0: SQLiteDatabase?) {
         val CREATE_TABLE = "CREATE TABLE $TABLE_NAME ($ID INTEGER PRIMARY KEY, $TASK_NAME TEXT, $TASK_DETAILS TEXT);"
-            p0?.execSQL(CREATE_TABLE)
+        p0?.execSQL(CREATE_TABLE)
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
         val DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
         p0?.execSQL(DROP_TABLE)
         onCreate(p0)
+    }
+
+    fun getAllTask(): List<TaskListModel>{
+        val tasklist = ArrayList<TaskListModel>()
+        val db = writableDatabase
+        val selectQuery = " SELECT * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(selectQuery,null)
+        if (cursor != null){
+            if (cursor.moveToFirst()){
+                do {
+                    val tasks = TaskListModel()
+                    tasks.id = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ID)))
+                    tasks.name = cursor.getString(cursor.getColumnIndex(TABLE_NAME))
+                    tasks.details = cursor.getString(cursor.getColumnIndex(TASK_DETAILS))
+                    tasklist.add(tasks)
+                }while (cursor.moveToNext())
+            }
+        }
+        cursor.close()
+        return tasklist
+    }
+
+    //insert
+    fun addTask(tasks : TaskListModel): Boolean{
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(TASK_NAME,tasks.name)
+        values.put(TASK_DETAILS,tasks.details)
+        val _success = db.insert(TABLE_NAME,null,values)
+        db.close()
+        return (Integer.parseInt("$_success") != -1)
+    }
+
+    //select the data of Particular id
+    fun getTask(_id: Int): TaskListModel{
+        val tasks = TaskListModel()
+        val db = writableDatabase
+        val selectQuery = "SLECT * FROM $TABLE_NAME WHERE 4ID = $_id"
+        val cursor = db.rawQuery(selectQuery,null)
+
+        cursor?.moveToFirst()
+        tasks.id = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ID)))
+        tasks.name = cursor.getString(cursor.getColumnIndex(TABLE_NAME))
+        tasks.details = cursor.getString(cursor.getColumnIndex(TASK_DETAILS))
+        cursor.close()
+        return tasks
+    }
+
+    fun deleteTask(_id: Int): Boolean{
+        val db = this.writableDatabase
+        val _success = db.delete(TABLE_NAME, ID + "?", arrayOf(_id.toString())).toLong()
+        db.close()
+        return Integer.parseInt("$_success") != -1
+    }
+
+    fun updateTask(tasks: TaskListModel): Boolean{
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(TASK_NAME,tasks.name)
+        values.put(TASK_DETAILS,tasks.details)
+        val _success = db.update(TABLE_NAME,values, ID + "=?", arrayOf(tasks.id.toString())).toLong()
+        db.close()
+        return Integer.parseInt("$_success") != -1
     }
 }
